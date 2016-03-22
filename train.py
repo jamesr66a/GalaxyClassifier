d6e270848843e2f579d49f854a14be15a978800c
James Reed <jamesr66@vt.edu>
2016-03-21 23:55:51 -0400

    Image I/O works

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import re
 import os
 
 import tensorflow as tf
+import numpy as np
 
 types = ['spiral', 'lenticular', 'irregular', 'elliptical']
 
@@ -24,16 +25,34 @@ examples = {}
 
 indices = {"spiral": 0, "lenticular": 1, "irregular": 2, "elliptical": 3}
 
-for t in types:
-  filename_queue = tf.train.string_input_producer(filenames[t])
+sess = tf.Session()
+with sess.as_default():
+  for t in types:
+    filename_queue = tf.train.string_input_producer(filenames[t])
+    print 'meme'
 
-  reader = tf.WholeFileReader()
-  key, value = reader.read(filename_queue)
+    reader = tf.WholeFileReader()
+    key, value = reader.read(filename_queue)
+    print 'lmao'
 
-  images[t] = tf.image.decode_jpeg(value, channels=3)
-  print images[t]
+    images[t] = tf.image.decode_jpeg(value, channels=3)
 
-  # Normalize pixels
-  images[t] = tf.image.per_image_whitening(images[t])
+    # Normalize pixels
+    images[t] = tf.image.per_image_whitening(images[t])
+    print 'dank'
 
-  # Add labels
+    # Add labels
+    #labels = tf.tile(tf.constant([indices[t]]), images[t].)
+
+  coord = tf.train.Coordinator()
+  threads = tf.train.start_queue_runners(coord=coord)
+
+  print 'abc'
+
+  for t in types:
+    for i in range(len(filenames[t])):
+      img = images[t].eval()
+      print img.shape
+
+  coord.request_stop()
+  coord.join(threads)
