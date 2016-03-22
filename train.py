4ba46c526ec989157aec82345a7ca71b19c01294
James Reed <jamesr66@vt.edu>
2016-03-22 02:06:37 -0400

    idk

--- train.py
+++ train.py
@@ -4,44 +4,38 @@ import tensorflow as tf
 
 types = ['spiral', 'lenticular', 'irregular', 'elliptical']
 
+def read_example(value):
+  record_defaults = [[""], [-1]]
+  path, label = tf.decode_csv(
+      value, record_defaults, " ")
 
-def read_example_map(example_map, type):
-  record_defaults=[tf.constant([], dtype=tf.string), tf.constant([], dtype=tf.int32)]
-  fn, label = tf.decode_csv(example_map, record_defaults, " ")
-  file_contents = tf.read_file(fn)
-  example = tf.image.decode_jpeg(file_contents, channels=3)
-  return example, label
+  image_file = tf.read_file(path)
+  image = tf.image.decode_jpeg(image_file, channels=3)
 
-def read_examples():
-  map_filenames = []
-  type_names = []
+  return image, label
 
-  for t in types:
-    map_filenames.append(os.path.join('./images', t, 'scaled', 'example_map'))
-    type_names.append(t)
 
-  filename_queue = tf.train.string_input_producer(map_filenames)
-  type_queue = tf.train.string_input_producer(type_names)
-
-  reader = tf.TextLineReader()
-  key, value = reader.read(filename_queue)
-
-  images_tensor, label_tensor = read_example_map(value, type_queue.dequeue())
-
-  #for t in next(iter(types)):
-    #example, label = read_example_map(input_queue.dequeue())
-    #images_tensor = tf.concat(1, images_tensor, example)
-    #label_tensor = tf.concat(1, label_tensor, label)
-
-  return images_tensor, label_tensor
-
-sess = tf.Session()
-with sess.as_default():
-  images, labels = read_examples()
+with tf.Session() as sess:
+  # Start populating the filename queue.
   coord = tf.train.Coordinator()
   threads = tf.train.start_queue_runners(coord=coord)
 
-  print images.eval(), labels.eval()
+  filenames = []
+  for t in types:
+    filenames.append(os.path.join('./images', t, 'scaled', 'example_map'))
+
+  filename_queue = tf.train.string_input_producer(filenames)
+
+  print filenames
+
+  reader = tf.WholeFileReader()
+  key, value = reader.read(filename_queue)
+
+  x = sess.run([value])
+  print x
+
+  #i, l = read_example(value)
+  #print sess.run([i, l])
 
   coord.request_stop()
   coord.join(threads)
